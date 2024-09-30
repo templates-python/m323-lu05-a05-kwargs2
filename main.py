90ece1fa722245eb643e4a75abf19f3335843306
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:06:41 +0200

    Ran pyink on branch main

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 def active_settings(**kwargs):
-        """
-        Returns a list of settings that are set to True.
+    """
+    Returns a list of settings that are set to True.
 
-        Parameters:
-            **kwargs: Variable number of keyword arguments representing settings.
+    Parameters:
+        **kwargs: Variable number of keyword arguments representing settings.
 
-        Returns:
-            list: A list containing the names of the settings that are set to True.
-        """
-        # TODO: Implementiere die Funktion
-        ...
+    Returns:
+        list: A list containing the names of the settings that are set to True.
+    """
+    # TODO: Implementiere die Funktion
+    ...
 
 
 if __name__ == '__main__':
     # Teste deine Funktion
-    print(active_settings(wifi=True, bluetooth=False, gps=True))  # Erwarteter Output: "[wifi, gps]"
+    print(
+        active_settings(wifi=True, bluetooth=False, gps=True)
+    )  # Erwarteter Output: "[wifi, gps]"
